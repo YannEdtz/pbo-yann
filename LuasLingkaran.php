f08dd9d8f8ea8b86d2f41006b357e7d3964fd992
Yann <101551375+YannEdtz@users.noreply.github.com>
2025-11-17 13:59:59 +0700

    Using method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,15 +1,9 @@
 <?php
 
-namespace App\Math; 
-
 class LuasLingkaran {
 
     public const phi = 3.14;
-    protected int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
-    }
+    public int $jari;
 
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
@@ -20,9 +14,10 @@ class LuasLingkaran {
         echo "<br/>";
         echo "ini dari static";
     }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "Selesai tanpa memulai";
-    }
 }
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
+
+LuasLingkaran::testing(); //panggil static method
